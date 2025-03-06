f9503e81d4a8a0f73b724a3449d4ff7db2a3827f
Sahib <sahib@Sahibs-MacBook-Air.local>
2025-03-06 11:26:30 -0500

    alter config file and env file

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Helps in serverless deployment
+  trailingSlash: true,
 };
 
 export default nextConfig;
